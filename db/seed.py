421969b13bce4af2ecadd0ebb5e80cf410e01501
Jackp325 <jack.patrick@gmail.com>
2026-06-17 17:13:11 +0100

    feat: seed venues table from JSON data

--- db/seed.py
+++ db/seed.py
@@ -9,11 +9,16 @@ def seed():
     """
     conn = get_connection()
 
-    drop_table(conn, "users")
+    tables = ["venues", "users"]
+    # Drop tables in order of dependencies
+    for table in tables:
+        drop_table(conn, table)
 
     create_users_table(conn)
+    create_venues_table(conn)
     
     insert_users(conn)
+    insert_venues(conn)
 
     conn.commit()
     conn.close()
@@ -42,12 +47,27 @@ def create_users_table(conn):
 
     cursor.close()
 
+def create_venues_table(conn):
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS venues(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            address TEXT,
+            capacity INT
+        );
+    """)
+
+    cursor.close()
+
+
 def insert_users(conn):
     cursor = conn.cursor()
 
     users = read_json("data/users.json")
 
-    user_records = [
+    user_info = [
         (
             user["name"],
             user["email"],
@@ -60,8 +80,33 @@ def insert_users(conn):
         INSERT INTO users (name, email, password)
         VALUES (%s, %s, %s)
         """,
-        user_records
+        user_info
     )
+
+    cursor.close()
+
+def insert_venues(conn):
+    cursor = conn.cursor()
+
+    venues = read_json("data/venues.json")
+
+    venue_info = [
+        (
+            venue["name"],
+            venue["address"],
+            venue["capacity"]
+        )
+        for venue in venues
+    ]
+    cursor.executemany(
+        """
+        INSERT INTO venues (name, address, capacity)
+        VALUES (%s, %s, %s)
+        """,
+        venue_info
+    )
+
+    cursor.close()
 
 if __name__ == "__main__":
     seed()
